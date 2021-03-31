8b51bc1190841b54b74903ed37ef80ca4ed45e5d
Raul Rodrigo Segura <raurodse@gmail.com>
2021-03-31 18:07:32 +0200

    add plugin file

---
+++ n4d/kerberos.py
@@ -0,0 +1,12 @@
+class kerberos:
+    def __init__(self):
+        # ficheros a controlar
+        # /etc/krb5.conf
+        # /etc/krb5kdc/kadm5.acl
+        # /etc/krb5kdc/kdc.conf
+        # /etc/idmapd.conf
+        # /etc/default/nfs-kernel-server
+        # /etc/default/nfs-common
+        # /var/lib/dnsmasq/hosts/reg
+        # /etc/exports.d/
+
